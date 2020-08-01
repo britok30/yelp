75fe47a4268f0eec010d0f1815d2ddf81bef5409
Kelvin Brito <britok30@gmail.com>
2020-08-01 14:40:35 -0400

    background + search bar

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,12 +2,16 @@ import React from 'react';
 import TopNav from '../Nav/TopNav';
 import logo from '../../assets/logo-foursquare.svg';
 import styles from './LandingPage.module.css';
+import SearchBar from '../SearchBar/SearchBar';
 
 const LandingPage = () => {
     return (
         <div>
-            <TopNav />
-            <img src={logo} className={styles.logo} alt="logo" />
+            <div className={styles.landing}>
+                <TopNav />
+                <img src={logo} className={styles.logo} alt="logo" />
+                <SearchBar />
+            </div>
         </div>
     );
 };
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styles from './SearchBar.module.css';
+
+const SearchBar = () => {
+    return (
+        <div>
+            <div className="field has-addons">
+                <p className="control">
+                    <button className="button is-static is-medium">
+                        Search
+                    </button>
+                </p>
+                <p className="control">
+                    <input
+                        className={`input is-medium ${styles['input-control']}`}
+                        type="text"
+                        placeholder="burgers, barbers, spas, handymen..."
+                    />
+                </p>
+                <p className="control">
+                    <button class="button is-static is-medium">NEAR</button>
+                </p>
+                <p className="control">
+                    <input
+                        className={`input is-medium ${styles['input-control']}`}
+                        type="text"
+                        placeholder="Where"
+                    />
+                </p>
+                <button
+                    className={`button is-medium ${styles['search-button']}`}
+                >
+                    <i className={`fas fa-search ${styles['search-icon']}`}></i>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default SearchBar;
