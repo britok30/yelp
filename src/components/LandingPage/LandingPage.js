425942bb9fec939b0e5bc4f3395582864a0cffff
Kelvin Brito <britok30@gmail.com>
2020-08-01 13:42:38 -0400

    add logo

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import TopNav from '../Nav/TopNav';
+import logo from '../../assets/logo-foursquare.svg';
+import styles from './LandingPage.module.css';
 
 const LandingPage = () => {
     return (
         <div>
             <TopNav />
+            <img src={logo} className={styles.logo} alt="logo" />
         </div>
     );
 };
