044a46ca25906ef663598e4cdb586d3ca9ab090c
Kelvin Brito <britok30@gmail.com>
2020-08-01 13:20:06 -0400

    styling nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
-    return <div className="App">Test</div>;
+    return (
+        <div className="App">
+            <LandingPage />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopNav from '../Nav/TopNav';
+
+const LandingPage = () => {
+    return (
+        <div>
+            <TopNav />
+        </div>
+    );
+};
+
+export default LandingPage;
