866f0e9dbcb8857165803bf02411e3609c70e34b
Kelvin Brito <britok30@gmail.com>
2020-08-02 19:36:08 -0400

    url config

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,8 +6,20 @@ import SearchBar from '../SearchBar/SearchBar';
 import Suggestions from '../Suggestions/Suggestions';
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-router-dom';
+import useReactRouter from 'use-react-router';
 
 const LandingPage = () => {
+    const { history } = useReactRouter();
+
+    const search = (term, location) => {
+        const urlEncodedTerm = encodeURI(term);
+        const urlEncodedLocation = encodeURI(location);
+
+        history.push(
+            `/search?find_desc=${urlEncodedTerm}&find_loc=${urlEncodedLocation}`
+        );
+    };
+
     return (
         <div className={styles.landing}>
             {/* <TopNav /> */}
@@ -18,7 +30,7 @@ const LandingPage = () => {
                     </Link>
                 </Fade>
                 <Fade bottom duration={3000} distance={'1rem'} delay={1000}>
-                    <SearchBar medium />
+                    <SearchBar medium search={search} />
                 </Fade>
 
                 <Suggestions />
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,43 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SearchBar.module.css';
 
-const SearchBar = ({ medium, shadow }) => {
+const SearchBar = ({ medium, shadow, search }) => {
+    const [term, setTerm] = useState('');
+    const [location, setLocation] = useState('');
+
     const isMedium = medium ? 'is-medium' : '';
     const isShadow = shadow ? styles.shadow : '';
-    
+
+    const onHandleSubmit = (e) => {
+        if (typeof search === 'function') {
+            search(term, location);
+        }
+        e.preventDefault();
+    };
+
     return (
         <div className={isShadow}>
-            <div className="field has-addons">
-                <p className="control">
-                    <button className={`button is-static ${isMedium}`}>
-                        Search
+            <form onSubmit={onHandleSubmit}>
+                <div className="field has-addons">
+                    <div className="control">
+                        <div className={`button is-static ${isMedium}`}>
+                            Search
+                        </div>
+                    </div>
+                    <p className="control">
+                        <input
+                            onChange={(e) => setTerm(e.target.value)}
+                            className={`input ${isMedium} ${styles['input-control']}`}
+                            type="text"
+                            placeholder="burgers, barbers, spas, handymen..."
+                            value={term}
+                        />
+                    </p>
+                    <div className="control">
+                        <div className={`button is-static ${isMedium}`}>
+                            NEAR
+                        </div>
+                    </div>
+                    <div className="control">
+                        <input
+                            onChange={(e) => setLocation(e.target.value)}
+                            className={`input ${isMedium} ${styles['input-control']}`}
+                            type="text"
+                            placeholder="Where"
+                            value={location}
+                        />
+                    </div>
+                    <button
+                        onClick={onHandleSubmit}
+                        className={`button ${isMedium} ${styles['search-button']}`}
+                    >
+                        <i
+                            className={`fas fa-search ${styles['search-icon']}`}
+                        ></i>
                     </button>
-                </p>
-                <p className="control">
-                    <input
-                        className={`input ${isMedium} ${styles['input-control']}`}
-                        type="text"
-                        placeholder="burgers, barbers, spas, handymen..."
-                    />
-                </p>
-                <p className="control">
-                    <button className={`button is-static ${isMedium}`}>
-                        NEAR
-                    </button>
-                </p>
-                <p className="control">
-                    <input
-                        className={`input ${isMedium} ${styles['input-control']}`}
-                        type="text"
-                        placeholder="Where"
-                    />
-                </p>
-                <button
-                    className={`button ${isMedium} ${styles['search-button']}`}
-                >
-                    <i className={`fas fa-search ${styles['search-icon']}`}></i>
-                </button>
-            </div>
+                </div>
+            </form>
         </div>
     );
 };
